0213b7e07ff5b9068bb7043e7da1046c0a808bdd
sueCalystegia00 <susan.basket0410@gmail.com>
2023-01-30 11:32:33 +0900

    change: replace tag after creating diff file

--- .bin/latexdiff-commit.sh
+++ .bin/latexdiff-commit.sh
@@ -1,2 +1,7 @@
+#!/bin/sh
+
 ## 任意のコミットIDとの差分を取る($1:コミットID, $2:ファイル名)
-latexdiff-vc -e utf8 -t CFONT --git --flatten --force --append-safecmd -r $1 $2
+latexdiff-vc -e utf8 -t CFONT --git --flatten --force --append-safecmd -r $1 ${2}.tex
+## latexdiffで生成した差分ファイル内の文字色の定義を置換する
+sed -i '' 's:{{\\protect\\color{blue} \\sf #1}}:{{\\protect\\color{red} #1}}:g' "${2}-diff${1}.tex"
+sed -i '' 's:{{\\protect\\color{red} \\scriptsize #1}}:{{}}:g' "${2}-diff${1}.tex"
--- .bin/latexdiff-lastTag.sh
+++ .bin/latexdiff-lastTag.sh
@@ -1,3 +1,8 @@
+#!/bin/sh
+
 ## 最新のgitタグとの差分を取る
 LAST_TAG=$(git tag | sort -V | tail -n 2 | xargs -n 1)
-latexdiff-vc -e utf8 -t CFONT --git --flatten --force --append-safecmd -r $LAST_TAG $1
+latexdiff-vc -e utf8 -t CFONT -s COLOR --git --flatten --force --append-safecmd -r $LAST_TAG ${1}.tex
+## latexdiffで生成した差分ファイル内の文字色の定義を置換する
+sed -i '' 's:{{\\protect\\color{blue} \\sf #1}}:{{\\protect\\color{red} #1}}:g' "${1}-diff${LAST_TAG}.tex"
+sed -i '' 's:{{\\protect\\color{red} \\scriptsize #1}}:{{}}:g' "${1}-diff${LAST_TAG}.tex"
